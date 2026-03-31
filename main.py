64220468cb52237ff0b30155a664bb13d22e9bb9
Wolfram8877 <baptiste.rouxel17@gmail.com>
2026-03-31 15:32:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-import flet as ft
+import flet as ft # Libraire web
 import numpy as np
-from PIL import Image, ImageDraw  
-import joblib
-import os
-from urllib.parse import urlparse
-import base64
+from PIL import Image, ImageDraw #Dessin sur l'image
+import joblib # Modèles de prédiction IA
+import os # Gestion de dossiers
+from urllib.parse import urlparse # Gestion d'url web
+import base64 #Affichage image
 import io  
 
 # Mot de passe Flet
-os.environ["FLET_SECRET_KEY"] = "super_cle_secrete_ph_123"
+os.environ["FLET_SECRET_KEY"] = "super_cle_secrete_ph_123" #Mot de passe nécessaire pour l'utilisation de Flet en serveur
 
-# Création du dossier temporaire pour les images du téléphone
+# Création du dossier temporaire pour les images sur le serveur
 if not os.path.exists("uploads"):
     os.makedirs("uploads")
 
 print("Chargement des IA sur le serveur...")
-# On charge les 4 modèles d'IA entrainés
+# Chargement des 4 modèles d'IA entrainés
 modele_knn = joblib.load('modele_knn_ph.pkl')
 modele_vectors = joblib.load('modele_vectors_ph.pkl')
 modele_RFR = joblib.load('modele_RFR_ph.pkl')
 modele_DTR = joblib.load('modele_DTR_ph.pkl')
 
-# --- FONCTION POUR TROUVER LE CENTRE ---
+# Fonction pour trouver le centre de l'image pour le rognage et le tracer du carré rouge
 def trouver_centre(image_pil):
     img_array = np.array(image_pil.convert('RGB'), dtype=np.int16)
     diff = img_array.max(axis=-1) - img_array.min(axis=-1)
@@ -35,24 +35,26 @@ def trouver_centre(image_pil):
         centre_x, centre_y = l // 2, h // 2
     return centre_x, centre_y
 
-# Cropping de l'image a centre (200x300 pixels)
+# Cropping de l'image au centre (200x300 pixels)
 def recadrer_image(image_pil, centre_x, centre_y): 
     return image_pil.convert('RGB').crop((centre_x - 100, centre_y - 150, centre_x + 100, centre_y + 150))
-
+    
+#Calcul du pH à partir des modèles
 def calculer_ph(image_decoupee):
     image_numpy = np.array(image_decoupee)
     moyenne_rgb = image_numpy.mean(axis=(0, 1)).reshape(1, -1)
     
-    # On fait les QUATRE prédictions avec les bons modèles
+    # Quatre prédictions sont faite avec les bons modèles
     ph_knn = round(float(modele_knn.predict(moyenne_rgb)[0]), 1)
     ph_vectors = round(float(modele_vectors.predict(moyenne_rgb)[0]), 1)
     ph_RFR = round(float(modele_RFR.predict(moyenne_rgb)[0]), 1)
     ph_DTR = round(float(modele_DTR.predict(moyenne_rgb)[0]), 1)
     
-    # On retourne les 4 résultats
     return ph_knn, ph_vectors, ph_RFR, ph_DTR
 
+#Fonction principale qui va gérer le comportement de l'application
 def main(page: ft.Page):
+    # Affichage graphique
     page.title = "pH Analyzer"
     page.horizontal_alignment = "center"
     page.theme_mode = "light"
@@ -68,11 +70,12 @@ def main(page: ft.Page):
     
     conteneur_image = ft.Container(width=300, height=300, border=ft.border.all(1, "grey"), border_radius=10)
 
+    # Réception des données du serveur, affichage et dessin sur l'image
     def on_upload(e: ft.FilePickerUploadEvent):
         if e.progress == 1.0: 
             chemin_fichier = os.path.join("uploads", e.file_name)
             
-            # MODIFICATION POUR LE RECTANGLE ---
+            # Dessin du rectangle
             original_image = Image.open(chemin_fichier)
             cx, cy = trouver_centre(original_image)
             
@@ -122,6 +125,7 @@ def main(page: ft.Page):
                 texte_resultat_knn.color = "red"
             page.update()
 
+    # Fonction qui s'occupe d'uploader la photo au serveur et tout les problèmes URL etc
     def on_result(e: ft.FilePickerResultEvent):
         if e.files:
             try:
@@ -155,9 +159,9 @@ def main(page: ft.Page):
 
     bouton = ft.ElevatedButton("Prendre une photo", on_click=lambda _: selecteur.pick_files())
 
-    # ON AJOUTE LES 4 TEXTES A LA PAGE ICI
+    # Affichage des 4 textes à la page
     page.add(titre, bouton, conteneur_image, texte_resultat_knn, texte_resultat_vectors, texte_resultat_RFR, texte_resultat_DTR)
 
-# LA LIGNE CLÉ POUR LE CLOUD RENDER :
+# Lancement du service de serveur Render avec la gestion du port etc
 port = int(os.environ.get("PORT", 8000))
 ft.app(target=main, host="0.0.0.0", port=port, upload_dir="uploads")
